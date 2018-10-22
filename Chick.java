cb6762f7bba1013fe5726957e8ebe540fd7b6f1a
EricY <eryu8@s.sfusd.edu>
2018-10-22 14:41:11 -0700

    Unkown gone

--- Chick.java
+++ Chick.java
@@ -1,4 +1,31 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+  private String myType;
+  private String mySound;
+  private String my2ndSound;
+  public Chick(String type, String sound1, String sound2)
+  {
+   myType = type;
+   mySound = sound1;
+   my2ndSound = sound2;
+   
+  }
+  public Chick() 
+  {
+    myType = "unknown";
+    mySound = "unknown";
+  }
+  public String getSound()
+  {
+    if(Math.random() > .5)
+    {
+    return mySound;
+    }
+    else
+    {
+    return my2ndSound;
+    }
+  }
+  public String getType(){return myType;}
+ 
 }
